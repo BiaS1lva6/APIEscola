6baff53c20d1c8744cf0c10f0e5de5d4a26f293f
Beatriz <beatriz080407@gmail.com>
2025-04-15 16:12:25 -0300

    fazendo uma Api escola

---
+++ APIEscola/Models/Matricula.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIEscola.Models
+{
+    public class Matricula
+    {
+        public Guid MatriculaId { get; set; } // Identificador único da matrícula
+
+        [Required(ErrorMessage = "O Campo Aluno é Obrigatório")] // Campo Obrigatório
+        public Guid AlunoId { get; set; } // Identificador do aluno associado à matrícula
+
+
+        [Required(ErrorMessage = "O Campo Turma é Obrigatório")] // Campo Obrigatório
+        public Guid TurmaId { get; set; } // Identificador da turma associada à matrícula
+
+        public Aluno? Aluno { get; set; } // Navegação para o aluno associado à matrícula
+        public Turma? Turma { get; set; } // Navegação para a turma associada à matrícula
+
+        [Required(ErrorMessage = "O Campo Data de Matricula é Obrigatório")]
+        [DataType(DataType.Date, ErrorMessage = "O Formato da Data não é Valido.")]
+        public DateTime DataMatricula { get; set; } = DateTime.Now;
+    }
+}
---
+++ APIEscola/Models/Turna.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIEscola.Models
+{
+    public class Turma
+    {
+        public Guid TurmaId { get; set; } // Identificador único da turma
+
+        [Required(ErrorMessage = "O Campo Sigla é Obrigatório")] // Campo Obrigatório
+        [MaxLength(10, ErrorMessage = "A Sigla deve ter no máximo 10 caracteres")] // Máximo de 10 caracteres
+        public string? Sigla { get; set; }
+
+        [Required(ErrorMessage = "O Campo Descrição é Obrigatório")] // Campo Obrigatório
+        [MaxLength(255, ErrorMessage = "A Descrição deve ter no máximo 255 caracteres")] // Máximo de 255 caracteres
+        public string? Descricao { get; set; }
+
+        [Required(ErrorMessage = "O Campo Data Final é Obrigatório")]
+        [StringLength(11, ErrorMessage = "O campo Data Final Deve ter 11 dígitos")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "O Campo Data Final deve conter apenas números")]
+        public DateOnly DataFim { get; set; } // Ano da turma
+
+        [Required(ErrorMessage = "O Campo Data inicio é Obrigatório")]
+        [StringLength(11, ErrorMessage = "O campo Data inicio Deve ter 11 dígitos")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "O Campo Data Inicio deve conter apenas números")]
+        public DateOnly DataInicio { get; set; } // Semestre da turma
+
+        [Required(ErrorMessage = "O Campo Curso é Obrigatório")] // Campo Obrigatório
+        public Guid CursoId { get; set; } // Identificador do curso associado à turma
+
+        public Curso? Curso { get; set; } // Navegação para o curso associado à turma
+    }
+}
